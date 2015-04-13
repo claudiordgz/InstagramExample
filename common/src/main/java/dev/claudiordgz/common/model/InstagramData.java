c9011c093d07df923fa3708525bbf5488b130a17
claudiordgz <claudio.rodriguez@premierlogic.com>
2015-04-12 20:24:31 -0400

    added common library for objects shared between libraries, will have to refactor into dependency injection later

--- common/src/main/java/dev/claudiordgz/common/model/InstagramData.java
+++ common/src/main/java/dev/claudiordgz/common/model/InstagramData.java
@@ -5,34 +5,35 @@ package dev.claudiordgz.common.model;
  * Created by Claudio on 3/31/2015.
  */
 public class InstagramData {
-    private String TAG = getClass().getName();
+  private String TAG = getClass().getName();
 
-    public String imageUrl;
-    public String username;
-    public String usernameProfileImage;
-    public String likesCount;
+  public String imageUrl;
+  public String username;
+  public String usernameProfileImage;
+  public String likesCount;
 
-    private int hashId = 0;
+  private int hashId = 0;
 
-    public int hashCode() {
-        if(hashId == 0){
-            String idStr = imageUrl.substring(imageUrl.lastIndexOf('/') + 1);
-            idStr = idStr.substring(0, idStr.lastIndexOf('.'));
-            idStr = idStr.replace("_", "");
-            idStr = idStr.replace("<", "");
-            idStr = idStr.replaceAll("[^\\d.]", "");
-            String specChar = "" +idStr.charAt(idStr.length()-10);
-            int idx = Integer.parseInt(specChar) == 1 ? 10 : 9;
-            idStr = idStr.substring(idStr.length()-idx);
-            hashId = Integer.parseInt(idStr);
-        }
-        return hashId;
+  public int hashCode() {
+    if (hashId == 0) {
+      String idStr = imageUrl.substring(imageUrl.lastIndexOf('/') + 1);
+      idStr = idStr.substring(0, idStr.lastIndexOf('.'));
+      idStr = idStr.replace("_", "");
+      idStr = idStr.replace("<", "");
+      idStr = idStr.replaceAll("[^\\d.]", "");
+      String specChar = "" + idStr.charAt(idStr.length() - 10);
+      int idx = Integer.parseInt(specChar) == 1 ? 10 : 9;
+      idStr = idStr.substring(idStr.length() - idx);
+      hashId = Integer.parseInt(idStr);
     }
-    public boolean equals(Object o) {
-        if (o instanceof InstagramData) {
-            InstagramData other = (InstagramData) o;
-            return (imageUrl.equals(other.imageUrl));
-        }
-        return false;
+    return hashId;
+  }
+
+  public boolean equals(Object o) {
+    if (o instanceof InstagramData) {
+      InstagramData other = (InstagramData) o;
+      return (imageUrl.equals(other.imageUrl));
     }
+    return false;
+  }
 }
